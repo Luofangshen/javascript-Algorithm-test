cb8201fbe62a5ae9cfaa742cbd3a76b607c80124
Paul <Paul@qq.com>
2018-09-27 12:53:42 +0800

    The twelfth day

--- free-camp-code-javascript/31.js
+++ free-camp-code-javascript/31.js
@@ -7,7 +7,11 @@
 
 这道题暂时解不出来。。。。。。。。。。以后再思考吧（应该是数学没学好= = 算法想不出来）
 
-要用到欧几里得算法解最小公倍数
+要用到欧几里得算法求出最大公约数
+定理：两个整数的最大公约数等于其中较小的那个数和两数相除余数的最大公约数。最大公约数（Greatest Common Divisor）缩写为GCD
+
+= =想到一种弱智的方法 ，全部乘积在循环 判断能整除全部的数得出直接输出,不过这个算法对于很大的数估计内存会溢出
+
  */
 
 function smallestCommons(arr) {
@@ -18,37 +22,36 @@ function smallestCommons(arr) {
   for (let i = a; i <= b; i++) {
     newArr.push(i)
   }
-  function prime(num) {
-    if (num === 1) {
-      return false
-    }
-    let flag = true
-    for (let i = 2; i < num; i++) {
-      if (num % i === 0) {
-        flag = false
+  function multiple(a, b) {
+    for (let i = 1; i <= a * b; i++) {
+      if (!(i % a) && !(i % b)) {
+        return i
       }
     }
-    return flag
+    return undefined
   }
-  newArr = newArr.map((num, index) => {
-    if (prime(num)) {
-      return num
-    } else {
-      let sum = 1
-      for (let i = 2; i < num; i++) {
-        if (prime(i) && num % i === 0) {
-          sum *= i
-        }
-      }
-      return sum
-    }
-  })
-  console.log(newArr)
-  newArr.forEach(num => {
-    sum *= num
-  })
-  console.log(sum)
-  return sum;
+  let mulArr = multiple(newArr[0], newArr[1])
+  console.log(mulArr)
+  for (let i = 2; i < newArr.length; i++) {
+    mulArr = multiple(mulArr, newArr[i])
+  }
+  console.log(mulArr)
+  return mulArr;
 }
 
-smallestCommons([1, 13]);
+smallestCommons([20, 1]);
+
+// 求最大公约数的算法 很简单 也很巧妙 说实话while循环真的好用 Vue源码也使用这个来提取Node到 documentfragment 在做批量修改及编译
+function gcd(a, b) {
+  if (a >= b) {
+    let c = a
+    a = b
+    b = c
+  }
+  while (b % a) {
+    let c = a
+    a = b % a
+    b = c
+  }
+  return a
+}
